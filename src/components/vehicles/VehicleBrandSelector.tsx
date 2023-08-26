811dd79ed27073f7a7710b78073d03e7d53fe6a4
pasindu-sandaruwan <pasindu.sliit32@gmail.com>
2023-08-26 20:18:02 +0530

    Implement the brand  selector

--- src/components/vehicles/VehicleBrandSelector.tsx
+++ src/components/vehicles/VehicleBrandSelector.tsx
@@ -1,5 +1,40 @@
-const VehicleBrandSelector = () =>{
-    return(
-        <div> The Brands </div>
-    )
-}
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { useState } from "react";
+
+const brandList: string[] = ["Volkswagen", "Audi", "Ford", "Mercedes", "BMW"];
+
+const VehicleBrandSelector = () => {
+    const [selectedBrand, setSelectedBrand] = useState("");
+    const handleChange = (e : any) => {
+        setSelectedBrand(e.target.value);
+    };
+
+    return (
+        <div
+            style={{
+                marginLeft: 10,
+            }}
+        >
+            <InputLabel id="demo-simple-select-label">Brand</InputLabel>
+            <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={selectedBrand}
+                label="Brand"
+                placeholder="Please select"
+                onChange={handleChange}
+            >
+                {brandList.map((brand: string, key: number) => {
+                    return (
+                        <MenuItem value={brand} key={key}>
+                            {brand}
+                        </MenuItem>
+                    );
+                })}
+            </Select>
+        </div>
+    );
+};
+export default VehicleBrandSelector;
--- src/components/vehicles/VehicleHeader.tsx
+++ src/components/vehicles/VehicleHeader.tsx
@@ -1,11 +1,12 @@
 import Grid from "@mui/material/Grid";
 import BiddingCartBadge from "../cart/BiddingCartBadge";
 import BiddingCartList from "../cart/BiddingCartList";
+import VehicleBrandSelector from "./VehicleBrandSelector";
 
 const VehicleHeader = () => {
     return <Grid container spacing={2}>
         <Grid item md = {8}>
-
+            <VehicleBrandSelector/>
         </Grid>
         <Grid item md = {4}>
             <BiddingCartBadge/>
